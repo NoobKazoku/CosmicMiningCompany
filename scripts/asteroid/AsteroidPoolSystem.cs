5e1c02c4633f35af21ffd3bdabb312b3e8c9071f
GwWuYou <95328647+GeWuYou@users.noreply.github.com>
2026-01-10 20:30:11 +0800

    feat(asteroid): 添加小行星对象池系统实现
    - 创建了 AbstractNodePool 抽象基类用于管理 Node2D 类型节点对象
    - 定义了 INodePool 和 IObjectPool 接口用于对象池标准操作
    - 实现了 AsteroidPoolSystem 类用于管理小行星对象的获取和回收
    - 提供了完整的对象池功能包括预加载、激活/停用、容量管理等特性
    - 添加了 IAsteroidPoolSystem 接口定义小行星对象池系统契约

---
+++ scripts/asteroid/AsteroidPoolSystem.cs
@@ -0,0 +1,19 @@
+﻿
+using CosmicMiningCompany.scripts.core;
+using Godot;
+
+namespace CosmicMiningCompany.scripts.asteroid;
+
+public class AsteroidPoolSystem
+    : AbstractNodePool<SpaceRock>, IAsteroidPoolSystem
+{
+    protected override PackedScene LoadScene()
+    {
+
+    }
+
+    public SpaceRock Acquire(Node2D parent)
+    {
+        throw new System.NotImplementedException();
+    }
+}
---
+++ scripts/asteroid/IAsteroidPoolSystem.cs
@@ -0,0 +1,23 @@
+﻿using GFramework.Core.Abstractions.system;
+using Godot;
+
+namespace CosmicMiningCompany.scripts.asteroid;
+
+/// <summary>
+/// 小行星对象池系统接口，用于管理小行星对象的获取和回收
+/// </summary>
+public interface IAsteroidPoolSystem : ISystem
+{
+    /// <summary>
+    /// 从小行星对象池中获取一个可用的小行星对象
+    /// </summary>
+    /// <param name="parent">要将获取的小行星添加到的父节点</param>
+    /// <returns>获取的SpaceRock对象实例</returns>
+    SpaceRock Acquire(Node2D parent);
+    
+    /// <summary>
+    /// 将使用完毕的小行星对象归还到对象池中
+    /// </summary>
+    /// <param name="rock">需要归还的小行星对象</param>
+    void Release(SpaceRock rock);
+}
